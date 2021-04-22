fbba2ff0ccf804df42a1c77394ed11d408d33eb3
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-04-21 23:39:25 -0500

    mostrar detalle de cada post y mostrar posts similares

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
